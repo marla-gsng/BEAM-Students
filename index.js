97df874df31ac16b9b38f429d7369ebb8979d610
marla-gsng <marlaisabelle.geesing@gmail.com>
2024-03-18 14:41:56 +0100

    installed and added .env to project

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 import chalk from "chalk";
 import http from "http";
+import "dotenv/config";
 
 // console.log(chalk.blueBright("Paul"));
 // console.log(chalk.redBright("Sheikh"));
 // console.log(chalk.greenBright("Wara"));
 // console.log(chalk.yellowBright("Sam"));
 
-const host = "localhost";
-const port = 3000;
+console.log(process.env);
+const host = process.env.HOST;
+const port = process.env.PORT;
 
 const server = http.createServer((request, response) => {
   response.end("<h1>Hello World</h1>");
