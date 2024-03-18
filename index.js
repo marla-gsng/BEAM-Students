c6e346ecafb44a6f23c36a6863fb5d3ad0911024
marla-gsng <marlaisabelle.geesing@gmail.com>
2024-03-18 13:39:57 +0100

    created server

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 import chalk from "chalk";
+import http from "http";
 
-console.log(chalk.blueBright("Paul"));
-console.log(chalk.redBright("Sheikh"));
-console.log(chalk.greenBright("Wara"));
-console.log(chalk.yellowBright("Sam"));
+// console.log(chalk.blueBright("Paul"));
+// console.log(chalk.redBright("Sheikh"));
+// console.log(chalk.greenBright("Wara"));
+// console.log(chalk.yellowBright("Sam"));
+
+const host = "localhost";
+const port = 3000;
+
+const server = http.createServer((request, response) => {
+  response.end("<h1>Hello World</h1>");
+});
+
+server.listen(port, host, () =>
+  console.log(chalk.greenBright(`Server is running on http://${host}:${port}`))
+);
